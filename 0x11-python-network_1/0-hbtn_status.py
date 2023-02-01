97f90a8bf44bffd0cfd473e52d0a3050f6050ee0
Ann <93719602+wangui-ann@users.noreply.github.com>
2023-02-02 00:58:30 +0300

    Update 0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
-""" Script that fetches https://intranet.hbtn.io/status
-with the usage of the package urllib"""
+"""Fetches data from given URL"""
 
 if __name__ == "__main__":
     import urllib.request
-    with urllib.request.urlopen('https://intranet.hbtn.io/status') as r:
-        html = r.read()
+
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status')\
+            as response:
+        html = response.read()
+
         print('Body response:')
         print('\t- type: {}'.format(type(html)))
         print('\t- content: {}'.format(html))
-        print('\t- utf8 content: {}'.format(html.decode('utf-8')))
+        print('\t- utf8 content: {}'.format(html.decode("utf-8", "replace")))
